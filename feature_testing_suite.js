810e338d7d0a29eaa03d9d5fbd065aee0b711e10
Dagmara <daga.sz@hotmail.com>
2016-11-07 17:01:36 +0000

    List of functions for feature testing suite

---
+++ feature_testing_suite.js
@@ -0,0 +1,29 @@
+function FeatureTest() = {};
+
+FeatureTest.prototype.visit = function (url) {
+
+};
+
+FeatureTest.prototype.fillIn = function (:field, :with) {
+
+};
+
+FeatureTest.prototype.clickButton = function(identifier){
+
+};
+
+FeatureTest.prototype.currentPath = function(){
+
+};
+
+FeatureTest.prototype.toHaveText = function(){
+
+};
+
+FeatureTest.prototype.statusCode = function () {
+
+};
+
+FeatureTest.prototype.toHaveLink = function () {
+  
+};

e668844e6dcc83046acc6dd278f12780e619ba65
cgulli <caitlin.gulliford@gmail.com>
2016-11-08 15:46:03 +0000

    working on click button to test fill in

--- feature_testing_suite.js
+++ feature_testing_suite.js
@@ -1,33 +1,42 @@
-function FeatureTest(){};
+function FeatureTest(){}
 
 FeatureTest.prototype.visit = function (url) {
   var xhr = new XMLHttpRequest();
   xhr.open("GET", url);
-  xhr.responseType = "document";
+  xhr.responseType = "text";
   xhr.send();
   return xhr;
 };
 
-FeatureTest.prototype.fillIn = function (field, content) {
-
+FeatureTest.prototype.fillIn = function (field, content, xhr) {
+  var parser = new DOMParser();
+  var xmlDoc = parser.parseFromString(xhr.responseText, "text/xml");
+  console.log(xmlDoc);
+  var input = xmlDoc.getElementsByName(field);
+  input.textContent = content;
 };
 
-FeatureTest.prototype.clickButton = function(identifier){
-
-};
-
-FeatureTest.prototype.currentPath = function(){
-
+FeatureTest.prototype.clickButton = function(identifier, xhr){
+  var parser = new DOMParser();
+  var xmlDoc = parser.parseFromString(xhr.responseText, "text/xml");
+  console.log(xmlDoc);
+  console.log(xmlDoc.getElementById("chad"));
+  xmlDoc.getElementById("chad").click();
 };
 
 FeatureTest.prototype.toHaveText = function(){
 
 };
 
-FeatureTest.prototype.statusCode = function () {
 
-};
+// FeatureTest.prototype.currentPath = function(){
+//
+// };
 
-FeatureTest.prototype.toHaveLink = function () {
+// FeatureTest.prototype.statusCode = function () {
+//
+// };
 
-};
+// FeatureTest.prototype.toHaveLink = function () {
+//
+// };
---
+++ views/script.js
@@ -0,0 +1,5 @@
+test = new FeatureTest();
+
+bob = test.visit('/Users/Home/Projects/notes-app/notes-app/views/testpage.html');
+
+test.clickButton("chad", bob);

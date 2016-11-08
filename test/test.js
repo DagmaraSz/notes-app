da1a737bff991773e2865a7160ddf2f2a22b2a92
Julio Gonzalez <julio.gteran@gmail.com>
2016-11-08 12:42:16 +0000

    Added css for unit testing

---
+++ test/test.js
@@ -0,0 +1,2 @@
+assert.isTrue(10 != 10)
+assert.toContain([1, 2 ,3], 2)
--- test/unit_testing_suite.js
+++ test/unit_testing_suite.js
@@ -1,53 +1,53 @@
 var assert =  {
   isTrue: function(assertionToCheck) {
     if (!assertionToCheck) {
-      document.write("Test Failed")
+      document.write("<div class=\"failed\">Test Failed</div>")
     }
     else {
-    	document.write("Test Passed")
+    	document.write("<div class=\"passed\">Test Passed</div>")
     }
   },
 
   isFalse: function(assertionToCheck) {
   	if (assertionToCheck) {
-  		document.write("Test Failed")
+  		document.write("<div class=\"failed\">Test Failed</div>")
   	}
 		else {
-			document.write("Test Passed")
+			document.write("<div class=\"passed\">Test Passed</div>")
 		}
   },
 
   toBe: function(currentValue, assertionToCheck) {
   	if (assertionToCheck === currentValue) {
-  		document.write("Test Passed")
+  		document.write("<div class=\"passed\">Test Passed</div>")
   	}
 		else {
-			document.write("Test Failed")
+			document.write("<div class=\"failed\">Test Failed</div>")
 		}
   },
 
   notToBe: function(currentValue, assertionToCheck) {
   	if (assertionToCheck === currentValue) {
-  		document.write("Test Failed")
+  		document.write("<div class=\"failed\">Test Failed</div>")
   	}
 		else {
-			document.write("Test Passed")
+			document.write("<div class=\"passed\">Test Passed</div>")
 		}
   },
 
   toContain: function(currentValue, assertionToCheck) {
   	if(currentValue.includes(assertionToCheck)) {
-  	  document.write("Test Passed")
+  	  document.write("<div class=\"passed\">Test Passed</div>")
   	} else {
-  		document.write("Test Failed")
+  		document.write("<div class=\"failed\">Test Failed</div>")
   	}
   },
 
   notToContain: function(currentValue, assertionToCheck) {
   	if(currentValue.includes(assertionToCheck)) {
-  	  document.write("Test Failed")
+  	  document.write("<div class=\"failed\">Test Failed</div>")
   	} else {
-  		document.write("Test Passed")
+  		document.write("<div class=\"passed\">Test Passed</div>")
   	}
   }
 };

1bce01b9c6721b39771da9394e7271e741d0320e
Dagmara <daga.sz@hotmail.com>
2016-11-09 18:11:13 +0000

    IT WORKSSSS. To do: refactor

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,34 @@
-function Model(){};
+function Model(){
+  this.notebook = new Notebook();
+};
 
 Model.prototype = {
   addNote: function(){
-    var header = this.getHeaderFromUrl();
-    var text = this.getHeaderFromUrl();
+    var header = this.getHeader();
+    var text = this.getText();
+    console.log(header)
+    console.log(text)
     note = new Note(header, text);
-
+    this.notebook.notes.push(note);
+    this.updateNoteList();
   },
-  getHeaderFromUrl: function() {
-    console.log(window.location);
+  getHeader: function() {
+    return document.getElementsByName("header")[0].value
+  },
+  getText: function() {
+    return document.getElementsByName("text")[0].value
+  },
+  updateNoteList(){
+    var node = document.createElement("DIV");
+    var headerNode = document.createElement("A");
+    var headerTextNode = document.createTextNode(this.notebook.notes[this.notebook.notes.length-1].header);
+    headerNode.appendChild(headerTextNode);
+    node.appendChild(headerNode);
+    var noteNode = document.createElement("P");
+    var noteTextNode = document.createTextNode(this.notebook.notes[this.notebook.notes.length-1].text);
+    noteNode.appendChild(noteTextNode);
+    node.appendChild(noteNode);
+    document.getElementById("left-content").appendChild(node);
   }
-
 
 };
--- test/spec/noteSpec.js
+++ test/spec/noteSpec.js
@@ -12,7 +12,6 @@ test.visit("http://localhost:8080/", function () {
   test.toHaveText("Add note");
 });
 
-
 note = new Note("First note", "bla bla");
 
 assert.toBe(note.header, "First note");

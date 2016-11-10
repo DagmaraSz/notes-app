70f2521ff32ff3c2aca5aca4029795a31d6e1a66
Julio Gonzalez <julio.gteran@gmail.com>
2016-11-10 12:31:31 +0000

    Notes text abreviation with text

--- public/test/spec/noteSpec.js
+++ public/test/spec/noteSpec.js
@@ -1,30 +1,16 @@
 var test = new FeatureTest()
 
-//tests the note exists
-
-test.visit("http://localhost:8080/", function () {
-  test.toHaveText("First note");
-  test.toHaveText("2nd note");
-});
-
-//tests adding a note
-document.getElementsByName("text").value = "Bala";
-document.getElementsByName("header").value = "Panda";
-document.getElementsByName("submit").click();
-if (document.getElementById("left-content").innerText.contains("Panda")){
-  console.log("Passed: Header present");
-} else {
-  console.log("Failed: Doesn't have header")
-}
-if (document.getElementById("left-content").innerText.contains("Bala")){
-  console.log("Passed: Content present");
-} else {
-  console.log("Failed: Doesn't have content")
-}
-
+// //tests the note exists
+//
+// test.visit("http://localhost:8080/", function () {
+//   test.toHaveText("First note");
+//   test.toHaveText("2nd note");
+// });
 
 //test for creating new note
-note = new Note("First note", "bla bla");
+note = new Note("First note", "Julio, Dagmara, Caitlin, Mike and Asif");
+
 
 assert.toBe(note.header, "First note");
-assert.toBe(note.text, "bla bla");
+assert.toBe(note.text, "Julio, Dagmara, Caitlin, Mike and Asif");
+assert.toBe(note.shortText, "Julio, Dagmara, Cait");

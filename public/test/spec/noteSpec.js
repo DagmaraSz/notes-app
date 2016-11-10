3a7f90d25875875cfe2c943623edefc51b507687
Julio Gonzalez <julio.gteran@gmail.com>
2016-11-10 12:41:13 +0000

    Closes #26

--- public/test/spec/noteSpec.js
+++ public/test/spec/noteSpec.js
@@ -1,15 +1,20 @@
 var test = new FeatureTest()
 
-// //tests the note exists
-//
-// test.visit("http://localhost:8080/", function () {
-//   test.toHaveText("First note");
-//   test.toHaveText("2nd note");
-// });
+//tests the note exists
+
+test.visit("http://localhost:8080/", function () {
+  test.toHaveText("First note");
+  test.toHaveText("2nd note");
+});
 
 //test for creating new note
-note = new Note("First note", "Julio, Dagmara, Caitlin, Mike and Asif");
 
+
+
+
+
+
+note = new Note("First note", "Julio, Dagmara, Caitlin, Mike and Asif");
 
 assert.toBe(note.header, "First note");
 assert.toBe(note.text, "Julio, Dagmara, Caitlin, Mike and Asif");

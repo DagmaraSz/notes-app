cf4cd029a75b5679a4e1de68dc7854f2ed51d706
LMCMLJ <mjcalvey2@gmail.com>
2016-11-10 15:32:44 +0000

    Adds two tests for links being properly created

--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,26 @@
 function Model(){
   this.notebook = new Notebook();
-};
+}
 
 Model.prototype = {
   addNote: function(){
     var header = this.getHeader();
     var text = this.getText();
-    console.log(header)
-    console.log(text)
     note = new Note(header, text);
     this.notebook.notes.push(note);
     this.updateNoteList();
   },
   getHeader: function() {
-    return document.getElementById("header").value
+    return document.getElementById("header").value;
   },
   getText: function() {
-    return document.getElementById("text").value
+    return document.getElementById("text").value;
   },
-  updateNoteList(){
+  updateNoteList: function() {
     var node = document.createElement("DIV");
-    var headerNode = document.createElement("A");
+    var headerNode = document.createElement("a");
+    var desiredLink = "#";
+    headerNode.setAttribute("href", desiredLink);
     var headerTextNode = document.createTextNode(this.notebook.notes[this.notebook.notes.length-1].header);
     headerNode.appendChild(headerTextNode);
     node.appendChild(headerNode);
@@ -30,5 +30,4 @@ Model.prototype = {
     node.appendChild(noteNode);
     document.getElementById("left-content").appendChild(node);
   }
-
 };
--- public/test/spec/noteSpec.js
+++ public/test/spec/noteSpec.js
@@ -15,14 +15,22 @@ window.onload = function() {
 	if (document.getElementById("left-content").innerText.includes("Panda")){
 	  console.log("Passed: Header present");
 	} else {
-	  console.log("Failed: Doesn't have header")
+	  console.log("Failed: Doesn't have header");
 	}
 	if (document.getElementById("left-content").innerText.includes("Bala")){
 	  console.log("Passed: Content present");
 	} else {
-	  console.log("Failed: Doesn't have content")
+	  console.log("Failed: Doesn't have content");
 	}
-}
+	if (document.getElementsByTagName("a")[0].innerHTML == "Panda") {
+		console.log("Passed: Header is a link");
+	} else {console.log("Failed: Header is not a link");
+	}
+	if (document.getElementsByTagName("a")[0].href == "http://localhost:8080/#") {
+		console.log("Passed: link is '#' ");
+	} else {console.log("Failed: link is not '#' ");}
+};
+
 
 note = new Note("First note", "Julio, Dagmara, Caitlin, Mike and Asif");
 

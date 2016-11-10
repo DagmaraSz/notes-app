a361570e3eaf50cead1fde14f68a775d66e86387
Asif Hafeez <asif.hafeez@hotmail.co.uk>
2016-11-10 12:31:29 +0000

    added first feature test

--- public/test/spec/noteSpec.js
+++ public/test/spec/noteSpec.js
@@ -1,25 +1,27 @@
-var test = new FeatureTest()
+// var test = new FeatureTest()
 
 //tests the note exists
 
-test.visit("http://localhost:8080/", function () {
-  test.toHaveText("First note");
-  test.toHaveText("2nd note");
-});
+// test.visit("http://localhost:8080/", function () {
+//   test.toHaveText("First note");
+//   test.toHaveText("2nd note");
+// });
 
 //tests adding a note
-document.getElementsByName("text").value = "Bala";
-document.getElementsByName("header").value = "Panda";
-document.getElementsByName("submit").click();
-if (document.getElementById("left-content").innerText.contains("Panda")){
-  console.log("Passed: Header present");
-} else {
-  console.log("Failed: Doesn't have header")
-}
-if (document.getElementById("left-content").innerText.contains("Bala")){
-  console.log("Passed: Content present");
-} else {
-  console.log("Failed: Doesn't have content")
+window.onload = function() {
+	document.getElementById("text").value = "Bala";
+	document.getElementById("header").value = "Panda";
+	document.getElementById("addNote").click();
+	if (document.getElementById("left-content").innerText.includes("Panda")){
+	  console.log("Passed: Header present");
+	} else {
+	  console.log("Failed: Doesn't have header")
+	}
+	if (document.getElementById("left-content").innerText.includes("Bala")){
+	  console.log("Passed: Content present");
+	} else {
+	  console.log("Failed: Doesn't have content")
+	}
 }
 
 
@@ -28,3 +30,4 @@ note = new Note("First note", "bla bla");
 
 assert.toBe(note.header, "First note");
 assert.toBe(note.text, "bla bla");
+
